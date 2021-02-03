68ab00b6e0031334901a9f7c919f5d2b4d5226c3
Aleksandr kozhevnykov <kozhevnykov.oleksandr@gmail.com>
2021-02-03 16:07:52 +0000

    Ad167 - add runtime

--- datapoint_generator.py
+++ datapoint_generator.py
@@ -3,6 +3,7 @@ import time
 import yaml
 import random
 import tarfile
+import argparse
 from subprocess import Popen
 
 
@@ -17,8 +18,7 @@ def create_stack_file(logs_path, config_path, tps=2000, validators=4):
         templates['services']['transaction']['volumes'][0] = logs_path + ':/mnt/logs'
 
         templates['services']['validator']['deploy']['replicas'] = validators
-
-        templates['services']['transaction']['command'] = 'bash -c "sleep 25 && python velas-ss/transaction_sender.py --tps '+\
+        templates['services']['transaction']['command'] = 'bash -c "sleep 15 && python velas-ss/transaction_sender.py --tps '+\
                                                    str(tps)+' --s 20 --host http://genesis_node"'
 
         with open(logs_path + '/docker-stack.yml', 'w') as fo:
@@ -26,23 +26,26 @@ def create_stack_file(logs_path, config_path, tps=2000, validators=4):
 
 
 def gzip_datapoint(b_path, output_name):
-    tar = tarfile.open(b_path + output_name + ".tar.gz", "w:gz")
+    tar = tarfile.open(b_path + 'archives/' + output_name + ".tar.gz", "w:gz", compresslevel=5)
     tar.add(b_path + output_name, arcname=output_name + 'tar')
     tar.close()
-
 
 
 def run_cluster(b_path, name):
     path = b_path + name + '/logs/'
     process1 = Popen('docker stack deploy -c '+path+'docker-stack.yml solana_stack', shell=True)
-    time.sleep(540)
+    time.sleep(1200)
     process2 = Popen('docker stack rm solana_stack', shell=True)
     time.sleep(30)
     gzip_datapoint(b_path, name)
 
 
-base_path = '/mnt/nfs_share/store1/solana/ad160/'
+parser = argparse.ArgumentParser(description='Run Solana-velas performance test')
+parser.add_argument('--start', default=1, type=int, help='first number of the experiment')
+parser.add_argument('--n', default=1, type=int, help='number of the experiment s')
+args = parser.parse_args()
 
+base_path = '/mnt/nfs_share/store2/solana/datapoints/'
 try:
     time.sleep(1)
     original_umask = os.umask(0)
@@ -51,14 +54,13 @@ try:
 except:
     pass
 
-for i in range(90, 91):
+for i in range(args.start, args.start + args.n):
     original_umask = os.umask(0)
     os.makedirs(base_path + str(i), mode=0o777)
     os.makedirs(base_path + str(i) + '/logs', mode=0o777)
     os.makedirs(base_path + str(i) + '/config', mode=0o777)
     os.umask(original_umask)
     create_stack_file(base_path + str(i) + '/logs', base_path + str(i) + '/config',
-                      tps=random.choice(range(1500, 8000, 500)), validators=random.choice(range(3, 7)))
+                      tps=random.choice(range(1000, 5000, 50)), validators=random.choice(range(3, 5)))
     time.sleep(2)
     run_cluster(base_path, str(i))
-
